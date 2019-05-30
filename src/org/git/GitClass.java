4fbde30d946eda2daa137861fa547fbd2a6be0df
HaripriyaaSaisubramanian <sharitpiyaa17@gmail.com>
2019-05-30 11:13:24 +0530

    adding to repository

---
+++ src/org/git/GitClass.java
@@ -0,0 +1,25 @@
+package org.git;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class GitClass {
+	public static void main(String[] args) {
+		List<Integer> set = new LinkedList();
+		set.add(10);
+		set.add(30);
+		set.add(40);
+		set.add(70);
+		set.add(100);
+		set.add(12);
+		System.out.println(set);
+		int size = set.size();
+		System.out.println(size);
+					
+		}
+
+	}
+	
+
+
